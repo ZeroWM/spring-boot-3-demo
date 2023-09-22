ebd579d24fc3156b90cc3008396987461a380711
ZeroWM <wm4968@gmail.com>
2023-09-22 18:58:04 +0800

    feat: fix h2 migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `actors` (`username` VARCHAR (255) NOT NULL,`display_name` VARCHAR (32) DEFAULT NULL,PRIMARY KEY (`username`)) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `actors` (`username` VARCHAR (255) NOT NULL,`display_name` VARCHAR (32) DEFAULT NULL,PRIMARY KEY (`username`));
